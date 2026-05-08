1074d2422aaaf9cb5617c94d82a969c94d690d45
ivandermansky <ivan.dermansky@gmail.com>
2026-05-08 17:56:47 +0300

    08-05-2026 BaseTestApi.java

--- src/main/java/BaseTestApi.java
+++ src/main/java/BaseTestApi.java
@@ -3,14 +3,17 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.BeforeClass;
 
 public class BaseTestApi {
-    protected static final String BASEURL = "https://qa-scooter.praktikum-services.ru";
+    protected static final String BASE_URL = "https://qa-scooter.praktikum-services.ru";
     protected static RequestSpecification requestSpec;
+    protected static final String LOGIN_ENDPOINT = "/api/v1/courier/login";
+
 
     @BeforeClass
     public static void setup() {
         requestSpec = new RequestSpecBuilder()
-                .setBaseUri(BASEURL)
+                .setBaseUri(BASE_URL)
                 .setContentType("application/json; charset=UTF-8")
                 .build();
     }
+
 }
