9c0c923a60f96a67573176f64036a32805934dcb
ivandermansky <ivan.dermansky@gmail.com>
2026-05-12 21:01:36 +0300

    12-05-26 AuthCourierAndDeleteTest.java

--- src/test/java/AuthCourierAndDeleteTest.java
+++ src/test/java/AuthCourierAndDeleteTest.java
@@ -9,70 +9,95 @@
  */
 
 
+package test;
+
 import Steps.CourierSteps;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
-public class AuthCourierAndDeleteTest extends CourierSteps {
+@Feature("Курьеры")
+public class AuthCourierAndDeleteTest {
 
-    private static final String LOGIN = "dvgfdsgwfswa";
+    private static final String LOGIN = "dvgfdsgwfswa1";
     private static final String PASSWORD = "password123";
     private static final String FIRST_NAME = "TestName";
 
+    // Создать экземпляр CourierSteps
+    private static CourierSteps courierSteps;
+
+    @BeforeClass
+    public static void setup() {
+        // Инициализировать requestSpec через метод setup в CourierSteps
+        CourierSteps.setup();
+        courierSteps = new CourierSteps();
+    }
+
     @Test
-    @Feature("Курьеры")
     @Description("Создание курьера с корректными учётными данными, авторизация и удаление")
     public void testCourierAuth() {
-        createAndVerifyCourier(LOGIN);
-        getCourierIdForCleanup(LOGIN);
+        int courierId = createAndVerifyCourier(LOGIN);
         performCourierAuth(LOGIN, PASSWORD);
+        deleteAndVerifyCourier(courierId);
     }
 
     @Step("Создание курьера с логином: {login}")
-    private void createAndVerifyCourier(String login) {
+    private int createAndVerifyCourier(String login) {
         String password = PASSWORD;
         String firstName = FIRST_NAME;
-        Response createResponse = createCourier(login, password, firstName);
-        checkResponse(createResponse, 201, true);
-    }
 
-    @Step("Получение ID курьера для удаления: {login}")
-    private void getCourierIdForCleanup(String login) {
-        courierIdToDelete = authorizeCourier(login, PASSWORD);
+        // Отправить запрос создания курьера через CourierSteps
+        Response createResponse = courierSteps.createCourier(login, password, firstName);
+
+        // Проверить ответ в тестовом классе
+        checkCreateCourierResponse(createResponse);
+
+        // Получить ID курьера для последующего удаления
+        Response authResponse = courierSteps.authorizeCourier(login, password);
+        courierSteps.checkSuccessfulAuthResponse(authResponse);
+        return authResponse.jsonPath().getInt("id");
     }
 
     @Step("Авторизация курьера: логин = {login}")
     private void performCourierAuth(String login, String password) {
-        authCourier(login, password);
+        // Отправить запрос авторизации через CourierSteps
+        Response authResponse = courierSteps.authorizeCourier(login, password);
+
+        // Проверить успешный ответ в тестовом классе
+        courierSteps.checkSuccessfulAuthResponse(authResponse);
     }
 
-    @Step("Проверка ответа: статус-код = {statusCode}, ok = {okValue}")
-    private void checkResponse(Response response, int statusCode, boolean okValue) {
+    @Step("Удаление курьера и проверка успешности")
+    private void deleteAndVerifyCourier(int courierId) {
+        if (courierId > -1) {
+            // Отправить запрос удаления через CourierSteps
+            Response deleteResponse = courierSteps.deleteCourierById(courierId);
+
+            // Проверить ответ удаления в тестовом классе
+            checkDeleteCourierResponse(deleteResponse);
+        }
+    }
+
+    @Step("Проверка ответа создания курьера: статус-код 201, ok = true")
+    private void checkCreateCourierResponse(Response response) {
         response.then()
-                .statusCode(statusCode)
-                .body("ok", equalTo(okValue));
+                .log().ifError()
+                .log().all()
+                .statusCode(201)
+                .body("ok", equalTo(true));
     }
 
-    @Step("Авторизация курьера - вызов для видимости в отчёте Allure: логин = {login}")
-    private void authCourier(String login, String password) {
-        given()
-                .spec(requestSpec)
-                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .formParams("login", login, "password", password)
-                .log().all()
-                .when()
-                .post(AUTH_COURIER_ENDPOINT)
-                .then()
+    @Step("Проверка ответа удаления курьера: статус-код 200, ok = true")
+    private void checkDeleteCourierResponse(Response response) {
+        response.then()
                 .log().all()
                 .statusCode(200)
-                .body("id", notNullValue())
-                .extract().response();
+                .body("ok", equalTo(true));
     }
 }
