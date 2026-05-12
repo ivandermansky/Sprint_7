72fd042d50a2b4d59fdb3a7cea28f2d108c69a2e
ivandermansky <ivan.dermansky@gmail.com>
2026-05-12 21:06:05 +0300

    12-05-26 AuthWithMissingFieldsTest.java

--- src/test/java/AuthWithMissingFieldsTest.java
+++ src/test/java/AuthWithMissingFieldsTest.java
@@ -4,14 +4,16 @@
 При тесте, в котором проверяется авторизация без пароля возвращается ошибка 504, что свидетельствует о баге: сервер некорректно обрабатывает null
 */
 
+package test;
+
+import Steps.CourierSteps;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
-import models.CourierTestData;
+import models.BaseTestApi;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 
 @Feature("Авторизация курьеров")
@@ -21,58 +23,44 @@ public class AuthWithMissingFieldsTest extends BaseTestApi {
     private static final String EXISTING_LOGIN = "182Blink182";
     private static final String EXISTING_PASSWORD = "182";
 
+    // Создать экземпляр CourierSteps
+    private CourierSteps courierSteps = new CourierSteps();
+
     @Test
-    @Description("Проверка ошибки при отсутствии поля 'login' в запросе авторизации")
+    @Description("Проверка ошибки при отсутствии поля login в запросе авторизации")
     public void testAuthWithoutLogin() {
-        // Создать данные для авторизации без логина
-        CourierTestData authData = new CourierTestData();
-        authData.setPassword(EXISTING_PASSWORD);
+        // Отправить запрос через метод из CourierSteps (метод принимает только пароль)
+        Response response = courierSteps.sendAuthRequestWithoutLogin(EXISTING_PASSWORD);
 
-        // Отправить запрос и проверить ответ через аннотированный метод
-        Response response = sendAuthRequestWithMissingLogin(authData);
+        // Проверить ответ в тестовом классе
+        checkAuthMissingLoginResponse(response);
     }
 
     @Test
-    @Description("Проверка ошибки при отсутствии поля 'password' в запросе авторизации")
+    @Description("Проверка ошибки при отсутствии поля password в запросе авторизации")
     public void testAuthWithoutPassword() {
-        // Создать данные для авторизации без пароля
-        CourierTestData authData = new CourierTestData();
-        authData.setLogin(EXISTING_LOGIN);
+        // Отправить запрос через метод из CourierSteps (метод принимает только логин)
+        Response response = courierSteps.sendAuthRequestWithoutPassword(EXISTING_LOGIN);
 
-        // Отправить запрос и проверить ответ через аннотированный метод
-        Response response = sendAuthRequestWithMissingPassword(authData);
+        // Проверить ответ в тестовом классе
+        checkAuthMissingPasswordResponse(response);
     }
 
-    @Step("Отправка запроса авторизации без поля 'login', пароль: '{password}'")
-    private Response sendAuthRequestWithMissingLogin(CourierTestData authData) {
-        return given()
-                .spec(requestSpec)
-                .body(authData)
-                .log().all()
-                .when()
-                .post(LOGIN_ENDPOINT)
-                .then()
+    @Step("Проверка ответа авторизации при отсутствии поля login: статус-код 400, сообщение об ошибке")
+    private void checkAuthMissingLoginResponse(Response response) {
+        response.then()
                 .log().ifError()
                 .log().all()
                 .statusCode(400)
-                .body("message", containsString("Недостаточно данных для входа"))
-                .extract().response();
+                .body("message", containsString("Недостаточно данных для входа"));
     }
 
-    @Step("Отправка запроса авторизации без поля 'password', логин: '{login}'")
-    private Response sendAuthRequestWithMissingPassword(CourierTestData authData) {
-        String login = authData.getLogin();
-        return given()
-                .spec(requestSpec)
-                .body(authData)
-                .log().all()
-                .when()
-                .post(LOGIN_ENDPOINT)
-                .then()
+    @Step("Проверка ответа авторизации при отсутствии поля password: статус-код 400, сообщение об ошибке")
+    private void checkAuthMissingPasswordResponse(Response response) {
+        response.then()
                 .log().ifError()
                 .log().all()
                 .statusCode(400)
-                .body("message", containsString("Недостаточно данных для входа"))
-                .extract().response();
+                .body("message", containsString("Недостаточно данных для входа"));
     }
 }
