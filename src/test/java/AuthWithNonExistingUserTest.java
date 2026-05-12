a3f58621cbb1abf9cee5c8df8db0bbc36e56743f
ivandermansky <ivan.dermansky@gmail.com>
2026-05-12 21:06:46 +0300

    12-05-26 AuthWithNonExistingUserTest.java

--- src/test/java/AuthWithNonExistingUserTest.java
+++ src/test/java/AuthWithNonExistingUserTest.java
@@ -3,14 +3,16 @@
 Для этого используется подбор рандомного логина с помощью currentTimeMillis()
 */
 
+package test;
+
+import Steps.CourierSteps;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
-import models.CourierTestData;
+import models.BaseTestApi;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 
 @Feature("Авторизация курьеров")
@@ -20,6 +22,9 @@ public class AuthWithNonExistingUserTest extends BaseTestApi {
     private static final String EXISTING_LOGIN = "182Blink182";
     private static final String EXISTING_PASSWORD = "182";
 
+    // Создать экземпляр CourierSteps
+    private CourierSteps courierSteps = new CourierSteps();
+
     @Test
     @Description("Проверка ошибки при авторизации с несуществующим логином")
     public void testAuthWithNonExistingLogin() {
@@ -27,30 +32,20 @@ public class AuthWithNonExistingUserTest extends BaseTestApi {
         String randomLogin = "nonexistent_" + System.currentTimeMillis();
         String password = "any_password"; // пароль может быть любым
 
-        // Формировать данные для авторизации с несуществующим логином
-        CourierTestData authData = new CourierTestData();
-        authData.setLogin(randomLogin);
-        authData.setPassword(password);
-
         System.out.println("Тестируемый логин (несуществующий): " + randomLogin);
 
-        // Отправить запрос на авторизацию через аннотированный метод
-        Response response = sendAuthRequestWithNonExistingUser(authData, randomLogin);
+        // Отправить запрос на авторизацию через метод из CourierSteps
+        Response response = courierSteps.sendAuthRequestWithNonExistingUser(randomLogin, password);
+
+        // Проверить ответ в тестовом классе
+        checkAuthNonExistingUserResponse(response);
     }
 
-    @Step("Отправка запроса авторизации с несуществующим логином '{login}' и паролем '{password}'")
-    private Response sendAuthRequestWithNonExistingUser(CourierTestData authData, String login) {
-        return given()
-                .spec(requestSpec)
-                .body(authData)
-                .log().all()
-                .when()
-                .post(LOGIN_ENDPOINT)
-                .then()
+    @Step("Проверка ответа авторизации с несуществующим пользователем: статус-код 404, сообщение об ошибке")
+    private void checkAuthNonExistingUserResponse(Response response) {
+        response.then()
                 .log().ifError()
-                .log().all()
                 .statusCode(404)
-                .body("message", containsString("Учетная запись не найдена"))
-                .extract().response();
+                .body("message", containsString("Учетная запись не найдена"));
     }
 }
